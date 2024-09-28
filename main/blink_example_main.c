3d090ee5c71df31f590b8e37a43284a7a80a29ab
Luong Huu Phuc <Huuphuc1016@gmail.com>
2024-09-28 15:09:41 +0700

    Update file local Repo

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -19,7 +19,7 @@ static const char *TAG = "example";
 /* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
    or you can edit the following line and set a number here.
 */
-#define BLINK_GPIO CONFIG_BLINK_GPIO
+#define BLINK_GPIO 2
 
 static uint8_t s_led_state = 0;
 
